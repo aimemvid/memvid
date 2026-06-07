745523b7482e748ed7f0c4e4a8042f24c72ee45c
aimemvid <aimemvid@gmail.com>
2026-06-08 01:40:56 +0700

    Update pages.js

--- konten/pages.js
+++ konten/pages.js
@@ -1,7 +1,6 @@
 // konten/pages.js
 
 import { home } from "./pages/home.js";
-import { roboprgama } from "./pages/roboprgama.js";
 import { tentangKami } from "./pages/tentang-kami.js";
 import { kontak } from "./pages/kontak.js";
 
@@ -18,7 +17,6 @@ export const SITE = {
 
 export const PAGES = {
   "/": home,
-  "/roboprgama": roboprgama,
   "/tentang-kami": tentangKami,
   "/kontak": kontak
 };
