eed1e5ae721f0101e608bbb7cf7f1d4d04837fe3
aimemvid <aimemvid@gmail.com>
2026-06-07 08:42:01 +0700

    Update head.js

--- templates/parts/head.js
+++ templates/parts/head.js
@@ -1,51 +1,33 @@
 // templates/parts/head.js
 
-export function head(CONFIG) {
+export function head(page) {
   return `
 <meta charset="UTF-8">
 
-<meta name="viewport"
-      content="width=device-width,initial-scale=1">
-
-<title>${CONFIG.title}</title>
+<title>${page.title}</title>
 
 <meta name="description"
-      content="${CONFIG.desc}">
+      content="${page.desc}">
 
 <link rel="canonical"
-      href="${CONFIG.canonical}">
-
-<link rel="amphtml"
-      href="${CONFIG.canonical}amp">
+      href="${page.canonical}">
 
 <link rel="icon"
-      href="${CONFIG.favicon}">
+      href="${page.favicon}">
+
+<meta property="og:title"
+      content="${page.title}">
+
+<meta property="og:description"
+      content="${page.desc}">
+
+<meta property="og:image"
+      content="${page.image}">
+
+<meta property="og:url"
+      content="${page.canonical}">
 
 <meta property="og:type"
       content="website">
-
-<meta property="og:title"
-      content="${CONFIG.title}">
-
-<meta property="og:description"
-      content="${CONFIG.desc}">
-
-<meta property="og:image"
-      content="${CONFIG.image}">
-
-<meta property="og:url"
-      content="${CONFIG.canonical}">
-
-<meta name="twitter:card"
-      content="summary_large_image">
-
-<meta name="twitter:title"
-      content="${CONFIG.title}">
-
-<meta name="twitter:description"
-      content="${CONFIG.desc}">
-
-<meta name="twitter:image"
-      content="${CONFIG.image}">
 `;
 }
