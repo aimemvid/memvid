d13dbf8b50d580e9884eab47868acc6c61567f34
aimemvid <aimemvid@gmail.com>
2026-06-07 07:50:17 +0700

    Update amp.js

--- functions/amp.js
+++ functions/amp.js
@@ -1,10 +1,20 @@
 // functions/amp.js
 
+import { PAGES } from "../konten/pages.js";
 import { ampTemplate } from "../templates/amp.js";
 
-export function ampPage(request) {
+export function ampPage(pathname = "/") {
+
+  const page = PAGES[pathname];
+
+  if (!page) {
+    return new Response("404 - Not Found", {
+      status: 404
+    });
+  }
+
   return new Response(
-    ampTemplate(),
+    ampTemplate(page),
     {
       headers: {
         "content-type": "text/html; charset=UTF-8"
